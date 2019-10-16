dd70afb216e60d19fbbffa689c8e28ef28bcbcb2
info <info@info13.cshl.edu>
2019-10-16 17:01:39 -0400

    adjusted find Pi program

--- Python2_PS/findPi.py
+++ Python2_PS/findPi.py
@@ -2,17 +2,32 @@
 import sys	# to read input from command line
 import math	# to use advanced math functions
 
-sampleNum = sys.argv[1]
-sampleNum = float(sampleNum)
-PI = 3.14159
+print('Would you like to play a game (Yes/No)?')
+ans = input()
+if ans == 'Yes' :
+	print('What is your best guess for pi?')	
+	sampleNum  = input()	
+		#	sampleNum = sys.argv[1]
+	sampleNum = float(sampleNum)
+	pi = 3.14
+	precPoor = 0.1
+	precGood = 0.01
 
-if sampleNum < PI :
-	output1 = 'Unfortunately,'
-	output2 = 'is less than pi :('
-elif sampleNum == PI :
-	output1 = 'You did it!'
-	output2 = 'is pi! You found pi! :)'
+	if round(abs(pi - sampleNum),2) > precPoor :
+		output1 = 'Not even close!'
+		output2 = 'is really far from pi!'
+		s = 'Your precision is off by > |{n}|!'
+		output3 = s.format(n=precPoor)
+	elif round(abs(pi - sampleNum),2) >=  precGood :
+		output1 = 'Not too bad.'
+		output2 = 'is not quite equal to pi.'
+		s = 'Your precision is off by >= |{n}|.'
+		output3 = s.format(n=precGood)
+	elif round(abs(pi - sampleNum),2) < precGood :
+		output1 = 'You did it!'
+		output2 = 'is pi! You found pi! :)'
+		s = 'Your precision is < |{n}|.'
+		output3 = s.format(n=precGood)
+	print(output1, sampleNum, output2, output3)
 else :
-	output1 = 'Unfortunately,'
-	output2 = 'is greater than pi :('
-print(output1, sampleNum, output2)
+	print('You are lame! Who doesn\'t like a game!')
