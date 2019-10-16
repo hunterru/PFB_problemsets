98177be585380104ccf8a7de733acc5c9cd7515d
info <info@info13.cshl.edu>
2019-10-16 11:00:25 -0400

    using input

--- Python1_PS/namePrint.py
+++ Python1_PS/namePrint.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+import sys
 
-print('My name: Hunter')
+name = sys.argv[1]
+color = sys.argv[2]
+activity = sys.argv[3]
+animal = sys.argv[4]
+
+print('My name:' , name)
+print('My favorite color:' , color)
+print('My favorite activity:' , activity)
+print('My favorite animal:' , animal)
