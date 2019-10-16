66bc7a32b665f96d78158c485db0e1ab9d0001d0
Hunter Underhill <hunter@Hunters-MacBook-Pro.local>
2019-10-16 12:27:22 -0400

    first try from laptop

--- Python1_PS/namePrint.py
+++ Python1_PS/namePrint.py
@@ -5,10 +5,8 @@ name = sys.argv[1]
 color = sys.argv[2]
 activity = sys.argv[3]
 animal = sys.argv[4]
-snack = sys.argv[5]
 
 print('My name:' , name)
 print('My favorite color:' , color)
 print('My favorite activity:' , activity)
 print('My favorite animal:' , animal)
-print('My favorite snack:' , snack)
