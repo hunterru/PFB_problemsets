604673c208703a02f11ba944d2985158c7991d2e
info <info@info13.cshl.edu>
2019-10-17 22:13:54 -0400

    more answers

--- Python4_PS/lenSeqPrint.py
+++ Python4_PS/lenSeqPrint.py
@@ -2,10 +2,8 @@
 import sys
 
 seq = ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
-count = 0
 for s in seq:
-	print(count, '\t', len(s), '\t', s)
-	count += 1
+	print(seq.index(s), '\t', len(s), '\t', s)
 print('Done!')
 
 temp = []
