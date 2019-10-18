bf3b1bfd60083d034653492d053d967c32fc3a4c
Hunter Underhill <hunter@Hunters-MacBook-Pro.local>
2019-10-18 08:47:20 -0400

    more problem answers

--- Python5_PS/dicFavs.py
+++ Python5_PS/dicFavs.py
@@ -9,10 +9,21 @@ things = {
 
 things['organism'] = 'Humans'
 
-print([key for key in things.keys()])
+# Using a string conditional to form a list of the keys
+opt = [key for key in things.keys()]
+opt_join = ', '.join(opt)
 
-print('What value from the following key would you like to see? Key:' , things.keys() )
+print('What value from the following key would you like to see? Key:' , opt_join)
 userIn = input()
 
 fav_thing = userIn
 print(things[fav_thing])
+
+# Changing the organism based on user input
+print('Now, let\'s change the organism. What is your favorite organism?')
+orgIn = input()
+things['organism'] = orgIn
+print('The following is your new dictionary')
+for key in things:
+	thing = things[key]
+	print("{:>15}".format(key), "{:<5}".format(thing))
