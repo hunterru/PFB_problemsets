96e5a2f6aba3b4826e1cd3f0479f9d3a2cd953f3
Hunter Underhill <hunter@Hunters-MacBook-Pro.local>
2019-10-19 08:47:32 -0400

    almost done

--- Python7_PS/enzCutList.py
+++ Python7_PS/enzCutList.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 import re
 
+enzDict = dict()
 with open('bionet.txt','r') as textFile:
 	text = textFile.readlines()[10:]
 	for line in text:
-		temp = re.search(r'(\S+\s{1,3}\S+)', line)
-		enz = temp.group(1)
-		print(enz)
-		
+		found = line.split(' ')
+		if found:
+			enz = found[0]+' '+found[1]
+			enz = enz.rstrip()
+			site = found[-1]
+			site = site.rstrip()
+			enzDict[enz] = site
+print(enzDict)
